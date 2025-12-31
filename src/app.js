4e3ad9fbf7ae0cf46882977ac211bf0b45ce1aa8
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-31 23:29:16 +0530

    refactor: Move main app file to src directory and implement initial database connection.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 dotenv.config();
 
-console.log(process.env.MONGODB_URI);
+
+// Database connection
+await connectDB()
+
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to AI Resume Builder API")
+})
 
 
 app.listen(PORT, () => {
