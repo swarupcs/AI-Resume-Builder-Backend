61c37669399d5c70da68a9c75ab663bba8aa40c0
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-12-31 23:36:27 +0530

    feat: initialize Express backend server with user authentication and retrieval endpoints.

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import authRouter from './routes/auth.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -19,6 +20,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Welcome to AI Resume Builder API")
 })
+
+app.use('/api/auth', authRouter)
 
 
 app.listen(PORT, () => {
